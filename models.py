69825fa9bba1772bc71080c1495f1779599af014
adiweb12 <vsadith051@gmail.com>
2026-02-25 10:20:48 +0530

    Refactor User model and add new fields
    Updated User model to change table name and added new fields.

--- models.py
+++ models.py
@@ -3,11 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__ = "users"   # Important: NOT "user"
+    __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
 
-    def __repr__(self):
-        return f"<User {self.email}>"
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(20))
+    password = db.Column(db.String(255), nullable=False)
+
+    otp_code = db.Column(db.String(6))
+    otp_expiry = db.Column(db.DateTime)
+    is_verified = db.Column(db.Boolean, default=False)
